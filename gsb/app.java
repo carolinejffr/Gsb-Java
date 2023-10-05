52bce3f8145c90666229f0b6b2d70afa0a6b19ca
Caroline Jaffré <120339746+carolinejffr@users.noreply.github.com>
2023-10-05 14:31:53 +0200

    ajout du sgdb custom

---
+++ gsb/app.java
@@ -0,0 +1,9 @@
+package gsb;
+
+public class app 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Bienvenue dans l'application GSB Java.");
+    }
+}
--- gsb/main.java
+++
@@ -1,5 +0,0 @@
-package gsb;
-
-public class main {
-    
-}
