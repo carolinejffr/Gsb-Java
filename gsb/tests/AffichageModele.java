969404eba815573f15c594dca577833a3b66a6cb
Caroline Jaffré <120339746+carolinejffr@users.noreply.github.com>
2023-10-05 16:45:53 +0200

    Ajout fenêtre de base
    Pour construire le GUI ensuite

--- gsb/tests/AffichageModele.java
+++ gsb/tests/AffichageModele.java
@@ -1,19 +1,21 @@
 package gsb.tests;
 
 import gsb.utils.sgdb;
+import gsb.vue.myFrame;
 
 public class AffichageModele 
 {
     public static void main(String[] args) 
     {
         System.out.println("Bienvenue dans l'application GSB Java.");
-        System.out.println("-----------------------------------");
-        System.out.println("Test de connexion :");
-        sgdb.utiliserSgdb("SELECT * FROM gsbv2.LOCALITE", true);
-        sgdb.utiliserSgdb("SELECT * FROM gsbv2.MEDECIN", true);
-        sgdb.utiliserSgdb("SELECT * FROM gsbv2.MEDICAMENT", true);
-        sgdb.utiliserSgdb("SELECT * FROM gsbv2.VISITE", true);
-        sgdb.utiliserSgdb("SELECT * FROM gsbv2.VISITEUR", true);
+        myFrame frame = new myFrame();
+        // System.out.println("-----------------------------------");
+        // System.out.println("Test de connexion :");
+        // sgdb.utiliserSgdb("SELECT * FROM gsbv2.LOCALITE", true);
+        // sgdb.utiliserSgdb("SELECT * FROM gsbv2.MEDECIN", true);
+        // sgdb.utiliserSgdb("SELECT * FROM gsbv2.MEDICAMENT", true);
+        // sgdb.utiliserSgdb("SELECT * FROM gsbv2.VISITE", true);
+        // sgdb.utiliserSgdb("SELECT * FROM gsbv2.VISITEUR", true);
 
 
 
